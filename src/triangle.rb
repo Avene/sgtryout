ac20e187609123384e20a55f551b00f5f5592315
Avene <juc-y@live.jp>
2015-05-20 22:07:24 +0900

    Add triangle recognition logic

--- src/triangle.rb
+++ src/triangle.rb
@@ -3,15 +3,20 @@ class Triangle
     return if argv[0] == "/vagrant/spec/triangle_spec.rb" # Workaround fof testing
 
     nums = parse_args(argv)
-    return nums
+
+    max = nums.max
+
+    return '三角形じゃないです＞＜' if max >= nums.inject(&:+) - max
+    return '正三角形ですね！' if nums.uniq.count == 1
+    return '二等辺三角形ですね！' if nums.uniq.count == 2
+    return '不等辺三角形ですね！'
   end
 
   def self.parse_args(argv)
     nums = argv.map(&:strip).map { |s| s.gsub(/,/, '') }
-    p nums
     raise StandardError.new('illegal num of args') unless nums.size == 3
     raise StandardError.new('illegal character') unless nums.count { |num| /\A\d+\z/.match num } == 3
-    nums
+    nums.map(&:to_i)
   end
 
   p recognize_shape(ARGV)
