679df2a25c30bf51f9a57257c02eb90f1f964f18
Avene <juc-y@live.jp>
2015-05-20 19:07:09 +0900

    Create rspec file i

---
+++ spec/triangle_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+
+describe 'Triangle' do
+
+  describe 'recognize an equilateral triangle' do
+
+  end
+
+  describe 'recognize a isosceles triangle' do
+
+  end
+
+  describe 'recognize a scalene triangle' do
+
+  end
+
+  describe 'recognize a non-triangle' do
+
+  end
+
+  describe  'raise error with incorrect arguments' do
+
+  end
+
+end
