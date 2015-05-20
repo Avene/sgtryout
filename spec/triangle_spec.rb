bcc353dc0d76122afe193ecee602c5cf89b2e032
Avene <juc-y@live.jp>
2015-05-20 20:30:11 +0900

    Add test code

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,37 +2,100 @@ require 'rspec'
 require_relative '../src/triangle'
 
 describe 'Triangle' do
+  subject { Triangle.recognize_shape(args) }
   describe 'recognize an equilateral triangle' do
-    subject {Triangle.recognize_shape(args)}
-    describe 'recognize arguments as equilateral triangle when three arguments are same value' do
-      let(:args){"3, 3, 3"}
-      it {is_expected.to eq '正三角形ですね！'}
+    context 'recognize arguments as equilateral triangle' do
+      describe 'when three arguments are same value' do
+        let(:args) { "3, 3, 3" }
+        it { is_expected.to eq '正三角形ですね！' }
+      end
     end
-    describe 'does not recognize arguments as equilateral triangle when two arguments are same value'
-    describe 'does not recognize arguments as equilateral triangle when all arguments are different value'
+
+    context 'does not recognize arguments as equilateral triangle' do
+      describe 'when two arguments are same value' do
+        let(:args) { "1, 3, 3" }
+        it { is_expected.not_to eq '正三角形ですね！' }
+      end
+      describe 'when all arguments are different value' do
+        let(:args) { "1, 2, 3" }
+        it { is_expected.not_to eq '正三角形ですね！' }
+      end
+    end
   end
 
   describe 'recognize a isosceles triangle' do
-    describe 'does not recognize arguments as isosceles triangle when three arguments are same value'
-    describe 'recognize arguments as isosceles triangle when two arguments are same value'
-    describe 'does not recognize arguments as isosceles triangle when all arguments are different value'
+    context 'recognize arguments as isosceles triangle' do
+      describe 'when two arguments are same value' do
+        let(:args) { "1, 3, 3" }
+        it { is_expected.to eq '二等辺三角形ですね！' }
+      end
+    end
+
+    context 'does not recognize arguments as isosceles triangle' do
+      describe 'when three arguments are same value' do
+        let(:args) { "3, 3, 3" }
+        it { is_expected.not_to eq '二等辺三角形ですね！' }
+      end
+      describe 'when all arguments are different value' do
+        let(:args) { "1, 2, 3" }
+        it { is_expected.not_to eq '二等辺三角形ですね！' }
+      end
+    end
   end
 
   describe 'recognize a scalene triangle' do
-    describe 'does not recognize arguments as isosceles triangle when three arguments are same value'
-    describe 'does not recognize arguments as isosceles triangle when two arguments are same value'
-    describe 'recognize recognize arguments as isosceles triangle when all arguments are different value'
+    context 'recognize recognize arguments as isosceles triangle' do
+      describe 'when all arguments are different value' do
+        let(:args) { "2, 3, 4" }
+        it { is_expected.to eq '不等辺三角形ですね！' }
+      end
+    end
+
+    context 'does not recognize arguments as isosceles triangle' do
+      describe 'when three arguments are same value' do
+        let(:args) { "3, 3, 3" }
+        it { is_expected.not_to eq '不等辺三角形ですね！' }
+      end
+      describe 'when two arguments are same value' do
+        let(:args) { "1, 3, 3" }
+        it { is_expected.not_to eq '不等辺三角形ですね！' }
+      end
+    end
   end
 
   describe 'recognize a non-triangle' do
-    describe 'recognize arguments as non-triangle when largest argument is equal to or larger than sum of other arguments'
-    describe 'does not recognize arguments as non-triangle when largest argument is less than sum of other arguments'
+    context 'recognize arguments as non-triangle' do
+      describe 'when largest argument is equal to or larger than sum of other arguments' do
+        let(:args) { "1, 2, 3" }
+        it { is_expected.to eq '三角形じゃないです＞＜' }
+      end
+    end
+
+    context 'does not recognize arguments as non-triangle' do
+      describe 'when largest argument is less than sum of other arguments' do
+        let(:args) { "1, 2, 4" }
+        it { is_expected.not_to eq '三角形じゃないです＞＜' }
+      end
+    end
   end
 
-  describe  'raise error wdescribeh incorrect arguments' do
-    describe 'raise error when arguments not formatted'
-    describe 'raise error when arguments contains 4 or more values'
-    describe 'raise error when arguments contains 2 or lesso values'
+  describe 'raise error with incorrect arguments' do
+    describe 'raise error when arguments not formatted' do
+      let(:args) { "123"}
+      it { is_expected.to raise_error StandardError }
+    end
+    describe 'raise error when arguments contains 4 or more values' do
+      let(:args) { "1, 2, 3, 4"}
+      it { is_expected.to raise_error StandardError }
+    end
+    describe 'raise error when arguments contains 2 or less values' do
+      let(:args) { "1,2, "}
+      it { is_expected.to raise_error StandardError }
+    end
+    describe 'raise error when arguments contains non-integer values' do
+      let(:args) { "1, 2, a"}
+      it { is_expected.to raise_error StandardError }
+    end
   end
 
 end
