7f397981ce5adff32d623b816173cdbea7ec751f
Avene <juc-y@live.jp>
2015-05-20 20:00:42 +0900

    Add triangle methods

--- src/triangle.rb
+++ src/triangle.rb
@@ -1,4 +1,15 @@
 
 class Triangle
+  def self.recognize_shape(argv)
+    validate_arguments(argv)
+    return argv
+  end
+
+  def self.validate_arguments(argv)
+    # code here
+  end
+
   p 'hello triangle'
+
+  p recognize_shape(ARGV)
 end
