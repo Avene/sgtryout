9d64653b6c8a5ed4cc4269c533b8c40210403a93
Avene <juc-y@live.jp>
2015-05-20 21:56:37 +0900

    Add validation method

--- src/triangle.rb
+++ src/triangle.rb
@@ -1,15 +1,18 @@
-
 class Triangle
   def self.recognize_shape(argv)
-    validate_arguments(argv)
-    return argv
+    return if argv[0] == "/vagrant/spec/triangle_spec.rb" # Workaround fof testing
+
+    nums = parse_args(argv)
+    return nums
   end
 
-  def self.validate_arguments(argv)
-    # code here
+  def self.parse_args(argv)
+    nums = argv.map(&:strip).map { |s| s.gsub(/,/, '') }
+    p nums
+    raise StandardError.new('illegal num of args') unless nums.size == 3
+    raise StandardError.new('illegal character') unless nums.count { |num| /\A\d+\z/.match num } == 3
+    nums
   end
-
-  p 'hello triangle'
 
   p recognize_shape(ARGV)
 end
